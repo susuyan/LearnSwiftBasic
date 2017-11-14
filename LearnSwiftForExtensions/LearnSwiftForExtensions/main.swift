fde2df7f30d982df930b970ee55ebae37e6f1bf6
susuyan <susuyan@163.com>
2017-11-14 18:05:40 +0800

    扩展

---
+++ LearnSwiftForExtensions/LearnSwiftForExtensions/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  LearnSwiftForExtensions
+//
+//  Created by susuyan on 2017/11/14.
+//  Copyright © 2017年 susuyan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+/*
+ 扩展就是为一个有的类、结构体、枚举类型或者协议类型添加新功能。
+ 这包括在没有权限获取原始源代码的情况下扩展类型的能力（即逆向建模）
+ 扩展和Objective-C中的分类类似，不同的是 Swift 的扩展没有名字
+ 
+ */
+
+
