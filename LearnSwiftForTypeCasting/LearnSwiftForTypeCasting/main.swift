bdfe90f40efe61033201f9d8ed43a7eba570a2c3
susuyan <susuyan@163.com>
2017-11-14 13:11:31 +0800

    类型转换

--- LearnSwiftForTypeCasting/LearnSwiftForTypeCasting/main.swift
+++ LearnSwiftForTypeCasting/LearnSwiftForTypeCasting/main.swift
@@ -61,6 +61,73 @@ for item in library {
 print("Meida library contains \(movieCount) movies and \(songCount) songs")
 
 // MARK: - 向下转型
+/*
+ 某类型的一个常量或变量可能在幕后实际上属于一个子类
+ 当确定是这种情况时，你可以尝试向下转到它的子类型，用类型转换操作符（as？或 as！）
+ 条件形式 as？返回一个你试图向下转成的类型的可选值
+ 强制形式 as！把试图向下转型或强制解包
+ */
+
+for item in library {
+    if let movie = item as? Movie {
+        print("Movie:'\(movie.name)',dir.\(movie.director)")
+    }else if let song = item as? Song{
+        print("Song:'\(song.name)',by \(song.artist)")
+    }
+}
+
+// MARK: - Any 和 AnyObject 的类型转换
+/*
+ Swift 为不确定类型提供了两种特殊的类型别名
+ Any 可以表示任何类型，包括函数类型
+ AnyObject 可以表示任何类类型的实例
+ */
+
+var things = [Any]()
+
+things.append(0)
+things.append(0.0)
+things.append(42)
+things.append(3.14159)
+things.append("hello")
+things.append((3.0, 5.0))
+things.append(Movie(name: "Ghostbusters", director: "Ivan Reitman"))
+things.append({ (name: String) -> String in "Hello, \(name)" })
+
+
+for thing in things {
+    switch thing {
+    case 0 as Int:
+        print("zero as an Int")
+        
+    default:
+        print("something else")
+    }
+    
+}
+
+/*
+ 注意：
+ Any 类型可以表示所有类型的值，包括可选型。Swift会在你用Any类型来表示一个可选值的时候，给你一个警告
+ 如果你确实想使用Any类型来承载可选值，你可以使用as操作符显示转换为Any
+  */
+let optionalNumber: Int? = 3
+//things.append(optionalNumber)  // 警告
+
+//things.append(optionalNumber as Any) // 没有警告
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
